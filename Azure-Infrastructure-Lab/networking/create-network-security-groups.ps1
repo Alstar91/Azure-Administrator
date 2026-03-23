68485aa7e8a1451e86237a6c96f9be9613613484
Aashish Chavan <aashish.chavan91@gmail.com>
2026-03-23 12:51:41 +0000

    Update create-network-security-groups.ps1

--- Azure-Infrastructure-Lab/networking/create-network-security-groups.ps1
+++ Azure-Infrastructure-Lab/networking/create-network-security-groups.ps1
@@ -9,7 +9,16 @@ for App, DB, and Management tiers.
 #>
 
 # Ensure connection
-Connect-AzAccount -UseDeviceAuthentication
+$subscription = Get-AzSubscription | Select-Object -First 1
+
+if (-not $subscription) {
+    Write-Host "Subscription Not Found." -ForegroundColor Red
+    return
+}
+
+Connect-AzAccount -Identity
+
+Set-AzContext -SubscriptionId $subscription.Id
 
 # =========================================
 
